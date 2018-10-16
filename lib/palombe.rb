a4483669317f1fc94f4290ede9f30f97612557e2
Yvan Sraka <yvan@sraka.pw>
2018-10-16 12:38:38 +0200

    v0.1.0

--- lib/palombe.rb
+++ lib/palombe.rb
@@ -1,9 +1,23 @@
+require "mkfifo"
+
 class Palombe
     def self.send name, value
-        puts "Send"
+        Dir.mkdir "/tmp/palombe/"
+        path = "/tmp/palombe/#{name}"
+        File.mkfifo(path)
+        File.open(path, "w") do |f|
+            f.puts value
+        end
     end
 
     def self.receive name
-        puts "Receive"
+        path = "/tmp/palombe/#{name}"
+        value = ""
+        File.open(path, "r") do |f|
+            while line = f.gets
+                value += line
+            end
+        end
+        value
     end
 end
--- palombe.gemspec
+++ palombe.gemspec
@@ -1,10 +1,11 @@
 Gem::Specification.new do |s|
     s.name        = 'palombe'
-    s.version     = '0.0.0'
+    s.version     = '0.1.0'
     s.summary = "Palombe lets you send and receive messages synchronously through different processes using named pipes"
     s.authors     = ["Yvan Sraka"]
     s.email       = 'yvan@sraka.pw'
     s.files       = ["lib/palombe.rb"]
     s.homepage    = 'http://rubygems.org/gems/palombe'
     s.license     = 'GPL-3.0'
-  end
+    s.add_dependency 'mkfifo', '~> 0.1.1'
+end
